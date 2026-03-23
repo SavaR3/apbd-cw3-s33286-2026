40f990b67cdb9f256aa0a31b46ff86c59d118de4
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-23 22:37:49 +0100

    update user abstract  class with properties and constructor

--- apbd-cw3-s33286-2026/src/Model/Users/User.cs
+++ apbd-cw3-s33286-2026/src/Model/Users/User.cs
@@ -2,5 +2,17 @@
 
 public abstract class User
 {
-    
+    public Guid Id { get; } = Guid.NewGuid();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public abstract int MaxRentals { get; }
+    public abstract string UserType { get; }
+
+    protected User(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string FullName => $"{FirstName} {LastName}";
 }
