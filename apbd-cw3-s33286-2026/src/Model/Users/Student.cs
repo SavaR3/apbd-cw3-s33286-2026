0ca0697479ea87562be70dc75cc9da08d0ac60ca
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-23 22:40:33 +0100

    update employee, student with constructor and properties

--- apbd-cw3-s33286-2026/src/Model/Users/Student.cs
+++ apbd-cw3-s33286-2026/src/Model/Users/Student.cs
@@ -2,5 +2,13 @@
 
 public class Student: User
 {
-    
+    public string StudentId { get; set; }
+    public override int MaxRentals => 2;
+    public override string UserType => "Student";
+
+    public Student(string firstName, string lastName, string studentId)
+        : base(firstName, lastName)
+    {
+        StudentId = studentId;
+    }
 }
