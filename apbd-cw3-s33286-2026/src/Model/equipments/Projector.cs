a287b162c9b7554a5aff57e529caa5459e830f68
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-23 22:15:40 +0100

    update camera, laptop, projector - constructors and properties

--- apbd-cw3-s33286-2026/src/Model/equipments/Projector.cs
+++ apbd-cw3-s33286-2026/src/Model/equipments/Projector.cs
@@ -2,5 +2,13 @@
 
 public class Projector: Equipment
 {
-    
+    public int Lumens {get; set;}
+    public string MaximumScreenSize {get; set;}
+    public string PixelResolution { get; set; }
+    public Projector(string name, int lumens, string maximumScreenSize, string pixelResolution) : base(name)
+    {
+        Lumens = lumens;
+        MaximumScreenSize = maximumScreenSize;
+        PixelResolution = pixelResolution;
+    }
 }
