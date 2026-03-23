7a9d759c15f0aab996909623e3b9f99e07bb0c00
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-23 23:11:42 +0100

    add rental model with properties, constructor, completeRental method

--- apbd-cw3-s33286-2026/src/Model/Rental.cs
+++ apbd-cw3-s33286-2026/src/Model/Rental.cs
@@ -2,5 +2,28 @@
 
 public class Rental
 {
-    
+    public Guid Id { get; } = Guid.NewGuid();
+    public Guid UserId { get; }
+    public Guid EquipmentId { get; }
+    public DateTime RentedAt { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ReturnedAt { get; set; }
+    public decimal Fine { get; set; }
+
+    public bool IsActive => ReturnedAt == null;
+    public bool IsOverdue => IsActive && DateTime.Now > DueDate;
+
+    public Rental(Guid userId, Guid equipmentId, int days)
+    {
+        UserId = userId;
+        EquipmentId = equipmentId;
+        RentedAt = DateTime.Now;
+        DueDate = DateTime.Now.AddDays(days);
+    }
+
+    public void CompleteRental(decimal fine)
+    {
+        ReturnedAt = DateTime.Now;
+        Fine = fine;
+    }
 }
