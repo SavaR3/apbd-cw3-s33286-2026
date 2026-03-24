911e3d1701dcd9d2f5708d5efca96492caf0e3b8
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-24 20:40:42 +0100

    add EquipmentService for inventory management

--- apbd-cw3-s33286-2026/src/Service/EquipmentService.cs
+++ apbd-cw3-s33286-2026/src/Service/EquipmentService.cs
@@ -1,6 +1,22 @@
 ﻿namespace apbd_cw3_s33286_2026.Service;
+using apbd_cw3_s33286_2026.Model;
 
 public class EquipmentService
 {
+    private readonly Data _db;
+
+    public EquipmentService(Data db)
+    {
+        _db = db;
+    }
+
+    public void AddEquipment(Equipment equipment)
+    {
+        _db.EquipmentList.Add(equipment);
+    }
+
+    public IEnumerable<Equipment> GetAllEquipment() => _db.EquipmentList;
     
+    public IEnumerable<Equipment> GetAvailableEquipment() => 
+        _db.EquipmentList.Where(e => e.IsAvailable);
 }
