91c7fb5a56c48ed556cac02eaa9eba4a57babe16
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-24 20:41:37 +0100

    add UserService for user registration and management

--- apbd-cw3-s33286-2026/src/Service/UserService.cs
+++ apbd-cw3-s33286-2026/src/Service/UserService.cs
@@ -1,6 +1,19 @@
 ﻿namespace apbd_cw3_s33286_2026.Service;
 
+using apbd_cw3_s33286_2026.Model;
+
 public class UserService
 {
+    private readonly Data _db;
+
+    public UserService(Data db)
+    {
+        _db = db;
+    }
+
+    public void RegisterUser(User user) => _db.Users.Add(user);
+
+    public List<User> GetAllUsers() => _db.Users;
     
+    public User? GetById(Guid id) => _db.Users.FirstOrDefault(u => u.Id == id);
 }
