1bc4d9b5e72f434adb2830c2478b7f57c53a30ee
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-24 20:37:51 +0100

    add data class to saving data

---
+++ apbd-cw3-s33286-2026/src/Service/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace apbd_cw3_s33286_2026.Service;
+using apbd_cw3_s33286_2026.Model;
+
+public class Data
+{
+    public List<User> Users { get; } = new();
+    public List<Equipment> EquipmentList { get; } = new();
+    public List<Rental> Rentals { get; } = new();
+}
